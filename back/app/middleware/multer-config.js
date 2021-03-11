4846638066a8cb7e0435f510e54e4921cc47051e
derfy86 <bourigeaudfrederic@yahoo.fr>
2021-03-11 19:04:27 +0100

    back/test prod

--- back/app/middleware/multer-config.js
+++ back/app/middleware/multer-config.js
@@ -12,7 +12,7 @@ const MIME_TYPES = {
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     //callback(null, 'storage file')
-    callback(null, 'images');
+    callback(null, 'assets/images');
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
